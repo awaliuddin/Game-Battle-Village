a4d9494da5288d4285e58cd193bc87cd0c046c75
Asif Waliuddin <asif.waliuddin@gmail.com>
2025-02-22 20:55:45 -0600

    Add Vercel deployment configuration

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ from app import app
 
 logging.basicConfig(level=logging.DEBUG)
 
+# For Vercel deployment
+app = app
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
