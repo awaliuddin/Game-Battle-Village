e32d46b9931cddbc52989959f897933b67d403d0
Asif Waliuddin <asif.waliuddin@gmail.com>
2025-02-22 21:32:15 -0600

    Update serverless configuration for Vercel deployment

---
+++ api/index.py
@@ -0,0 +1,4 @@
+from main import app
+
+# For Vercel serverless deployment
+handler = app
--- main.py
+++ main.py
@@ -7,5 +7,9 @@ logging.basicConfig(level=logging.DEBUG)
 # For Vercel deployment
 app = app
 
+# This is required for Vercel
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+else:
+    # For serverless environment
+    app.debug = False
